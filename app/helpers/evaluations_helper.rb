12c82fc4ca58f8624c15dcb1ec80d7efac5005cd
Edoardo Morassutto <gianluigi988@gmail.com>
2015-05-10 11:19:18 +0200

    Small refactor

--- app/helpers/evaluations_helper.rb
+++ app/helpers/evaluations_helper.rb
@@ -20,9 +20,10 @@ EOC
   end
 
   def new_evaluation_button(teacher, student_id, type_id)
+    href = new_evaluation_teacher_path(teacher, student_id: student_id, type_id: type_id)
     html = <<EOC
 <span class="evaluation-box-container pull-right">
-  <a class="evaluation-box btn btn-sm btn-primary" href="#{new_evaluation_teacher_path(teacher, student_id: student_id, type_id: type_id)}">
+  <a class="evaluation-box btn btn-sm btn-primary" href="#{href}">
     <div class="evaluation-box-content">
       <div class="evaluation-box-add">
         Add
