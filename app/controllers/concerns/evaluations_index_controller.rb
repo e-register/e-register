ba57a5610ad6cf841a787d89b45958415c4ff64b
Edoardo Morassutto <gianluigi988@gmail.com>
2015-05-13 22:56:02 +0200

    Small refactor to EvaluationsIndexController

--- app/controllers/concerns/evaluations_index_controller.rb
+++ app/controllers/concerns/evaluations_index_controller.rb
@@ -20,28 +20,31 @@ class EvaluationsIndexController
 
   def index_student
     students = current_user.students.includes(:klass)
-    if students.count == 1
-      @base.redirect_to @base.evaluations_student_path(students.first)
-    else
-      @base.instance_variable_set :@students, students
-      @base.render 'index_student'
-    end
+    do_index(students, 'student')
   end
 
   def index_teacher
     teachers = current_user.teachers.includes(:klass, :subject)
-    if teachers.count == 1
-      @base.redirect_to @base.evaluations_teacher_path(teachers.first)
-    else
-      @base.instance_variable_set :@teachers, teachers
-      @base.render 'index_teacher'
-    end
+    do_index(teachers, 'teacher')
   end
 
   def index_admin
     teachers = Teacher.includes(:klass, :user, :subject).
         to_a.sort_by { |x| x.klass.name }.group_by { |x| x.klass }
     @base.instance_variable_set :@teachers, teachers
-    @base.render 'index_admin'
+    render 'index_admin'
+  end
+
+  def do_index(collection, model)
+    if collection.count == 1
+      redirect_to @base.send("evaluations_#{model}_path", collection.first)
+    else
+      @base.instance_variable_set("@#{model.pluralize}", collection)
+      render "index_#{model}"
+    end
+  end
+
+  def method_missing(*arg, &blk)
+    @base.send(*arg, &blk)
   end
 end
